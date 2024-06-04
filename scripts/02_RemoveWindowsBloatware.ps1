cd5291978028727c9b8c1d3a70961de5722c9d79
Bob Walker <38642825+BobJWalker@users.noreply.github.com>
2024-06-03 19:09:45 -0500

    Update 02_RemoveWindowsBloatware.ps1

--- scripts/02_RemoveWindowsBloatware.ps1
+++ scripts/02_RemoveWindowsBloatware.ps1
@@ -21,6 +21,7 @@ Get-AppxPackage -AllUsers Microsoft.XboxGamingOverlay | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.XboxIdentityProvider | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.Xbox.TCUI | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
+Get-AppxPackage -Allusers Microsoft.XboxGamingOverlay | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.BingWeather | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.WindowsSoundRecorder | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.ScreenSketch | Remove-AppxPackage
@@ -28,3 +29,19 @@ Get-AppxPackage -AllUsers Microsoft.YourPhone | Remove-AppxPackage
 Get-AppxPackage -AllUsers MSTeams | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.Getstarted | Remove-AppxPackage
 Get-AppxPackage -AllUsers MicrosoftTeams | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.MicrosoftJournal | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.MixedReality.Portal | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.WindowsAlarms | Remove-AppxPackage
+
+
+## For my home PCs
+Get-AppxPackage -AllUsers AD2F1837.HPEnhance | Remove-AppxPackage
+Get-AppxPackage -AllUsers AD2F1837.HPInc.EnergyStar | Remove-AppxPackage
+Get-AppxPackage -AllUsers AD2F1837.HPPCHardwareDiagnosticsWindows | Remove-AppxPackage
+Get-AppxPackage -AllUsers AD2F1837.HPPrinterControl | Remove-AppxPackage
+Get-AppxPackage -AllUsers AD2F1837.HPPrivacySettings | Remove-AppxPackage
+Get-AppxPackage -AllUsers AD2F1837.HPSystemEventUtility | Remove-AppxPackage
+Get-AppxPackage -AllUsers AD2F1837.HPThermalControl | Remove-AppxPackage
+Get-AppxPackage -AllUsers AD2F1837.myHP | Remove-AppxPackage
+Get-AppxPackage -AllUsers AD2F1837.OMENCommandCenter | Remove-AppxPackage
+Get-AppxPackage -AllUsers C27EB4BA.DropboxOEM | Remove-AppxPackage
